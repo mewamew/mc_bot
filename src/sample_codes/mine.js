e9233bf1cf43149713a80a576d3021d7c8cd6c67
unknown <huazi@123.com>
2024-11-19 19:20:31 +0800

    准备进行调试

--- src/sample_codes/mine.js
+++ src/sample_codes/mine.js
@@ -1,2 +1,5 @@
-// 只能2格范围内挖掘指定数量的方块, 所以在调用时请先检查附近是否有目标方块
-await utils.mineBlock('oak_log', 4);
+// 64格范围内挖掘指定数量的方块, 所以在调用时请先检查附近是否有目标方块
+await utils.mineBlock('oak_log', 4, 64);
+await utils.mineBlock('coal_ore', 4, 64);
+//挖矿结束后，如果想返回地面，可以调用以下代码
+await utils.returnToGround();
--- src/skills/learned/20b71ef3-2372-4822-965a-0c7e7d81e2c3.js
+++
@@ -1,9 +0,0 @@
-async function mineOakLogs(bot) {
-    // 移动到最近的 oak_log
-    await utils.moveTo({x: -7.50, y: 173.00, z: -7.39});
-    logger.report('已移动到 oak_log 的位置', bot);
-    
-    // 挖掘 oak_log
-    await utils.mineBlock('oak_log', 3);
-    logger.report('已成功挖掘3个 oak_log', bot);
-}
